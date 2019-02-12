91688bfe26be3edc278a21e4aee749788c8d8ff7
HoweverC <852181000@qq.com>
2019-02-12 16:04:24 +0800

    Create LeetCode-824.py

---
+++ LeetCode-824.py
@@ -0,0 +1,13 @@
+class Solution:
+    def toGoatLatin(self, S: 'str') -> 'str':
+        vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+        words = S.split(' ')
+        new_words = []
+        for i, word in enumerate(words):
+            if word[0] in vowels:
+                word += 'ma'
+            else:
+                word = word[1:] + word[0] + 'ma'
+            word += 'a' * (i + 1)
+            new_words.append(word)
+        return ' '.join(new_words)
