9380997b02c1527865da1f3b07dad906c4897671
HoweverC <852181000@qq.com>
2019-02-22 21:26:55 +0800

    Create LeetCode-976.py

---
+++ LeetCode-976.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largestPerimeter(self, A: 'List[int]') -> 'int':
+        A.sort(reverse=True)
+        for i in range(len(A) - 2):
+            if A[i+1] + A[i+2] > A[i]:
+                return A[i] + A[i+1] + A[i+2]
+        return 0
