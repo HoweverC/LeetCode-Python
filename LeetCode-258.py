0afe7af70f4db8a6f913a292bdc3d182f18dc051
HoweverC <852181000@qq.com>
2019-03-08 21:47:23 +0800

    Update LeetCode-258.py

--- LeetCode-258.py
+++ LeetCode-258.py
@@ -1,9 +1,9 @@
 class Solution:
     def addDigits(self, num: int) -> int:
         while num > 9:
-            sum = 0
+            s = 0
             while num >= 1:
-                sum += num % 10
+                s += num % 10
                 num //= 10
-            num = sum
+            num = s
         return num
